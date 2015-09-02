432040efe7906656d5800f4aa2ecd87316169439
Sulu Fiti <fiti.sulu@gmail.com>
2015-09-02 16:57:17 +1200

    update config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  puts "key", ENV['FACEBOOK_KEY']
-  puts "secret"
   provider :facebook, ENV['FACEBOOK_KEY'],  ENV['FACEBOOK_SECRET']
 
 end
