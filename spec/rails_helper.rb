e91f48e7e6886e9c1e78041ecfbfe2fdd94e19d9
Ryan Virtue & Sulu Fiti <pair+ryanvirtue.sulufiti@devbootcamp.com>
2015-09-02 15:20:06 +1200

    Add login test
    To get Module working you need to create a support folder in spec.
    In this folder you create a helper with the module.
    In rails_helper require this file. Don't forget to change require in
    test file. Config rails_helper with config.include Name of the module.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,11 +25,11 @@ require 'rspec/rails'
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-
+require_relative './support/omni_auth_test_helper'
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include OmniAuthTestHelper
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
