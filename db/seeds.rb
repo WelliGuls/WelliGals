a90444836242b06b80a372f074d8636362e4a319
Anna Lezhikova <anna.v.l@gmail.com>
2015-09-15 20:55:58 +1200

    add meetup seed to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Meetup.create(title: "Houswarming party",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing
+               elit. Pellentesque suscipit justo iaculis lorem feugiat malesuada.
+              Sed bibendum iaculis arcu, quis ultricies sem pellentesque at.
+               Fusce lobortis tincidunt risus, vel mollis leo. Sed tempus
+               placerat eleifend. Ut dictum gravida magna, nec viverra felis
+               varius eu. Curabitur molestie non tortor et eleifend. Sed
+               bibendum tincidunt purus, nec euismod dolor congue id. Nam
+               eleifend, eros ut rutrum posuere, turpis metus condimentum purus,
+               in tincidunt justo neque at turpis. Praesent ut egestas elit.
+              Donec bibendum pretium justo vitae suscipit. Donec nec lacus
+              eleifend tellus porttitor malesuada.",
+              date: '20 September 2015',
+              place: "Anna's place",
+              capacity: 20,
+              user_id: 1
+  )
