c440902fac67d64455cd6390e2806eefcef9f468
Anna Lezhikova <anna.v.l@gmail.com>
2015-09-22 15:32:52 +1200

    Add validation to meetup model

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,3 +1,11 @@
 class Meetup < ActiveRecord::Base
   belongs_to :user
+
+  validates :title, presense: true, length: { in: 5..20 }
+  validates :description, presense: true, length: { maximum: 250 }
+  validates :date, :place, presense: true
+  validates :capacity, presense: true, numericality: { only_integer: true, greater_than: 0 }
+
+
+
 end
