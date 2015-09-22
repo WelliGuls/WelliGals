c3ac360aa440b25e51a19990fe517da77515d7ce
Anna Lezhikova <anna.v.l@gmail.com>
2015-09-22 14:05:17 +1200

    sets up basic app with login to facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,4 @@
 class User < ActiveRecord::Base
-#   def self.sign_in_from_omniauth(auth)
-#     find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
-#   end
-
-#   def self.create_user_from_omniauth(auth)
-#     create(
-#       provider: auth['provider'],
-#       uid: auth['uid'],
-#       name: auth['info']['name'],
-#       )
-#   end
-# end
 
   def self.find_or_create_from_omniauth(auth)
     user = User.where({:uid => auth[:uid], :provider => auth[:provider]}).first_or_create
@@ -19,7 +7,6 @@ class User < ActiveRecord::Base
     user.uid = auth['uid']
     user.provider = auth['provider']
     user.name = info["name"]
-    # user.github_token = auth["credentials"]["token"]
 
     user.save if user.changed?
     user
